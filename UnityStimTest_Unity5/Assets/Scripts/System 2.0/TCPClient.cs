77c1f6a0edc3c5a52e69a182ea86b165329ebdaa
conovich <conovich@gmail.com>
2015-11-06 15:56:49 -0500

    Reorganized project. Setup basic TCP communication test for switching to system 2.0.

---
+++ UnityStimTest_Unity5/Assets/Scripts/System 2.0/TCPClient.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+public class TCPClient : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		try {
+			TcpClient tcpclnt = new TcpClient();
+			Debug.Log("Connecting.....");
+			
+			tcpclnt.Connect("128.59.87.117",8001);
+			// use the ipaddress as in the server program
+			
+			Debug.Log("Connected");
+			Debug.Log("Enter the string to be transmitted : ");
+			
+			String str=Console.ReadLine();
+			Stream stm = tcpclnt.GetStream();
+			
+			ASCIIEncoding asen= new ASCIIEncoding();
+			byte[] ba=asen.GetBytes(str);
+			Debug.Log("Transmitting.....");
+			
+			stm.Write(ba,0,ba.Length);
+			
+			byte[] bb=new byte[100];
+			int k=stm.Read(bb,0,100);
+			
+			for (int i=0;i<k;i++)
+				Debug.Log(Convert.ToChar(bb[i]));
+			
+			tcpclnt.Close();
+		}
+		
+		catch (Exception e) {
+			Debug.Log("Error..... " + e.StackTrace);
+		}
+	}
+}
---
+++ UnityStimTest_Unity5/Assets/Scripts/System 2.0/TCPServer.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+using System.Threading;
+
+public class TCPServer : MonoBehaviour {
+
+	ThreadedServer myServer;
+
+	// Use this for initialization
+	void Start () {
+		myServer = new ThreadedServer ();
+		myServer.Start ();
+	}
+
+	void OnApplicationQuit(){
+		myServer.End ();
+		Debug.Log ("Ended server.");
+	}
+}
+
+public class ThreadedServer : ThreadedJob{
+	public bool isRunning = false;
+
+	public ThreadedServer(){
+
+	}
+
+	protected override void ThreadFunction()
+	{
+		isRunning = true;
+		// Do your threaded task. DON'T use the Unity API here
+		while (isRunning) {
+			ConnectClients();
+		}
+
+	}
+
+	void ConnectClients(){
+		try {
+			IPAddress ipAd = IPAddress.Parse("128.59.87.117");
+			// use local m/c IP address, and 
+			// use the same in the client
+			
+			/* Initializes the Listener */
+			TcpListener myList=new TcpListener(ipAd,8001);
+			
+			/* Start Listening at the specified port */        
+			myList.Start();
+			
+			Debug.Log("The server is running at port 8001...");    
+			Debug.Log("The local End point is  :" + 
+			          myList.LocalEndpoint );
+			Debug.Log("Waiting for a connection.....");
+
+			Socket s = myList.AcceptSocket();
+			
+			
+			
+			byte[] b=new byte[100];
+			int k=s.Receive(b);
+			Debug.Log("Recieved...");
+			for (int i=0;i<k;i++)
+				Debug.Log(Convert.ToChar(b[i]));
+			
+			ASCIIEncoding asen=new ASCIIEncoding();
+			s.Send(asen.GetBytes("The string was recieved by the server."));
+			Debug.Log("\nSent Acknowledgement");
+			/* clean up */            
+			s.Close();
+			myList.Stop();
+			
+		}
+		catch (Exception e) {
+			//Console.WriteLine("Error..... " + e.StackTrace);
+			Debug.Log("UH OH" + e.StackTrace);
+		}  
+	}
+
+	protected override void OnFinished()
+	{
+		// This is executed by the Unity main thread when the job is finished
+
+	}
+
+	public void End(){
+		isRunning = false;
+	}
+}
